7d3138b0a708759ee5b08aacd7681357bd66ba9f
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-09-07 22:01:20 +0300

    added option menu for a new note

--- app/src/main/java/com/naomi/notes/MainActivity.java
+++ app/src/main/java/com/naomi/notes/MainActivity.java
@@ -3,6 +3,9 @@ package com.naomi.notes;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -14,6 +17,29 @@ public class MainActivity extends AppCompatActivity {
 
       static ArrayList<String> notes = new ArrayList<>();
       static ArrayAdapter arrayAdapter;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        super.onOptionsItemSelected(item);
+
+        if (item.getItemId() == R.id.add_note) {
+
+            Intent intent = new Intent(getApplicationContext(), NoteEditorActivity.class);
+
+            startActivity(intent);
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
